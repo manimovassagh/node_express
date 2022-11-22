f0a61bba7dd4c5ee1bcbea6959768bd1a0e56497
manimovassagh <mani.mgh@gmail.com>
2022-11-22 17:04:04 +0100

    first

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const winston = require('winston');
+const expressWinston = require('express-winston');
+const app = express()
+app.use(expressWinston.logger({
+    transports: [
+      new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    meta: false,
+    msg: "HTTP  ",
+    expressFormat: true,
+    colorize: false,
+    ignoreRoute: function (req, res) { return false; }
+  }));
+
+
+const port = 3000
+const students =[
+    {
+        name:"Mani",
+        Age:24
+    },
+    {
+        name:"sahar",
+        Age:33
+    }
+]
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/students', (req, res) => {
+  res.send(students)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
